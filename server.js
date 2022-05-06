4966fa18ad954e9b18d7b74f69f012be72d8b9be
hachi-ops <elen20@protonmail.com>
2022-05-06 21:09:13 +0100

    changes

--- server.js
+++ server.js
@@ -2,22 +2,16 @@ const express = require("express");
 
 const app = express();
 
-// req is the Request object, res is the Response object
-// (these are just variable names, they can be anything but it's a convention to call them req and res)
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`Server started on ${PORT}`));
+
 app.get("/", function (req, res) {
   res.send("Hello World!");
 });
 
-const lists = new Map();
-const mailingList = mailingList.json();
-// see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
+const details = require("./lists.json");
 
-// add some fake data
-
-app.get("/lists", (req, res) => {
-  const listsArray = Array.from(lists.keys()); // Why is this like this? Try it out in your console.
-  res.send(listsArray);
-});
-app.listen(3000, function () {
-  console.log("Server is listening on port 3000. Ready to accept requests!");
+app.get("/details", function (request, response) {
+  response.json(details);
 });
